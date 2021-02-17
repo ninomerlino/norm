f5bc5760c78501edc4dcd39ec4885d73e51eec34
ninomerlino <lorenzomerlino9@gmail.com>
2021-02-17 16:46:41 +0100

    statistiche ram 1

--- linux_monitoring/linux_monitoring.cpp
+++ linux_monitoring/linux_monitoring.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
+#include <chrono>
 #include <string>
-#include <map>
 #include <vector>
 //#include <python3.8/Python.h>
 
@@ -11,6 +11,8 @@ unsigned int cpu_core_count;
 vector<double> cpu_freq_max;
 vector<double> cpu_freq_min;
 vector<double> cpu_term_max;
+int            ram_size_max;
+string         ram_unit;
 
 //data handling
 string get_data_as_string(string cmd, unsigned int max_buffer = 64) {
@@ -51,11 +53,39 @@ string vtos(vector<double> v){
   data += "]";
   return data;
 }
+string get_data_from_columns_as_string(string cmd){
+  string data = "[";
+  FILE * stream;
+  char letter;
+  bool end = false;
+  cmd.append(" 2>&1");
+  stream = popen(cmd.c_str(), "r");
+  if (stream) {
+    while (!feof(stream) && !ferror(stream)){
+      letter = fgetc(stream);
+      if(letter == ' ' || letter == ':')end = true;
+      else if(letter == '\n'){}
+      else{
+        if(end){
+          end = false;
+          data.push_back(',');
+        }
+        data.push_back(letter);
+      }
+    }
+  }
+  data.pop_back();
+  pclose(stream);
+  data += "]";
+  return data;
+}
+
 void data_init(){
     cpu_freq_max = get_data_as_double("cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq");
     cpu_freq_min = get_data_as_double("cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq");
     cpu_term_max = get_data_as_double("cat /sys/class/thermal/cooling_device*/max_state");
     cpu_core_count = cpu_freq_max.size();
+    ram_unit = "mega";
     is_init = true;
 }
 //cpu function
@@ -98,15 +128,43 @@ string get_cpu_temperature(){
     return get_data_as_string("cat /sys/class/thermal/thermal_zone*/temp");
 }
 //memory
-
+string get_ram_info(){
+  return "["+ get_data_from_columns_as_string("free --"+ram_unit+" | grep Mem")
+  + "," + get_data_from_columns_as_string("free --"+ram_unit+" | grep Swap") + "]";
+}
 //gpu
 //net
 //storage
+//debug class
+class Timer{
+  chrono::time_point<chrono::system_clock> start_time;
+  public:
+  Timer(){
+    start_time = chrono::system_clock::now();
+  }
+  void lap(string lap_name = "lap"){
+    cout<<lap_name<<" : ";
+    stop();
+    restart();  
+  }
+  void restart(){
+    start_time = chrono::system_clock::now();
+  }
+  void stop(){
+    cout<<chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now() - start_time).count()<<" ms"<<endl;
+  }
+};
+
 int main(){
   data_init();
+  Timer t = Timer();
+  t.lap();
   cout<<get_cpu_static_info()<<endl;
+  t.lap("static");
   cout<<get_cpu_temperature()<<endl;
-  cout<<get_cpu_cooling_state()<<endl;
   cout<<get_cpu_usage()<<endl;
+  t.lap("cpu");
+  cout<<get_ram_info()<<endl;
+  t.lap("ram");
   return 0;
 }
