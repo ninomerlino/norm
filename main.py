4f6b825d75b2d6e49fb14a82ec455fd87cb7a942
ninomerlino <lorenzomerlino9@gmail.com>
2021-02-26 14:26:04 +0100

    check dependecies before launching the program

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from sys import argv
+from sys import argv, modules
 from norm.routes import app
 from werkzeug.serving import run_simple
+
+def check_dependecies() -> bool:
+    dependecies = None
+    run = True
+    with open("requirements.txt","r") as file:
+        dependecies = file.read().split("\n")
+    for dep in dependecies:
+        if not (dep in modules):
+            print(f"The module {dep} is not installed \n\tplease run 'python -m pip install {dep}'")
+            run = False
+    return run
 
 def flag_check(flag : str, default):
     if flag in argv:
@@ -8,16 +19,17 @@ def flag_check(flag : str, default):
         if i+1 < len(argv) and not ('-' in argv[i+1]):
             return argv[i+1]
     return default
-#flag check
-host = flag_check('-h', '0.0.0.0')
-port = flag_check('-p', 5000)
-workers = flag_check('-w',1)
-debug = False
-if '-d' in argv:
-    debug = True
-if '-ns' in argv:
-    ssl = None
-else:
-    ssl = 'adhoc'
 
-run_simple(application=app, hostname=host, port=int(port), use_debugger=debug, ssl_context=ssl, processes=int(workers))
+#program
+if check_dependecies() and __name__ == '__main__':
+    host = flag_check('-h', '0.0.0.0')
+    port = flag_check('-p', 5000)
+    workers = flag_check('-w',1)
+    debug = False
+    if '-d' in argv:
+        debug = True
+    if '-ns' in argv:
+        ssl = None
+    else:
+        ssl = 'adhoc'
+    run_simple(application=app, hostname=host, port=int(port), use_debugger=debug, ssl_context=ssl, processes=int(workers))
